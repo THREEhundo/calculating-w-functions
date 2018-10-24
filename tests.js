cb0487b0ef6e6c8efd230a03462e850ffca7b742
THREEhundo <35667805+THREEhundo@users.noreply.github.com>
2018-10-24 16:09:30 -0400

    added a problem & test file.

---
+++ tests.js
@@ -0,0 +1,6 @@
+// Sample Tests
+
+Test.assertEquals(seven(times(five())), 35);
+Test.assertEquals(four(plus(nine())), 13);
+Test.assertEquals(eight(minus(three())), 5);
+Test.assertEquals(six(dividedBy(two())), 3);
